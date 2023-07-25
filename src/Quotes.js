bfe3ac9d6122647e216f33a2849c445878e47aa8
AppolinFotso <appolin.s.fotso@gmail.com>
2023-07-25 08:30:59 +0100

    Removed ReactDom from child components

--- src/Quotes.js
+++ src/Quotes.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { nanoid } from "nanoid";
 
 function Quotes(prop) {
--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { useState } from "react";
 
 function Search(prop) {
